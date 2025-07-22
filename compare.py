374bc8e2e6d5420676e9755032eeba0fe5760805
neha-c23 <neha.chatterjee@bp.com>
2025-07-22 15:23:05 +0530

    Update compare.py

--- compare.py
+++ compare.py
@@ -19,6 +19,6 @@ def compare_parameters(maps_template_url, quickstart_template_url):
 
 # Call the function with URLs
 compare_parameters(
-    "https://raw.githubusercontent.com/neha-c23/arm_validation/refs/heads/main/templates/DeploymentTemplate_ASEv3_Generic.json",
+    "https://raw.githubusercontent.com/neha-c23/arm_validation/refs/heads/main/templates/DeploymentTemplate_ASP_Generic.json",
     "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/4b98edf26a59f311fe599bef4e48850d55e5fb50/quickstarts/microsoft.web/web-app-asp-app-on-asev3-create/azuredeploy.json"
 )
