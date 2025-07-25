53dc9b084e4cacdc09b9999b27ac02306f1a7a3f
neha-c23 <neha.chatterjee@bp.com>
2025-07-25 11:58:39 +0530

    Update compare.py

--- compare.py
+++ compare.py
@@ -5,7 +5,7 @@ def load_parameters_from_url(url):
     with urllib.request.urlopen(url) as response:
         data = response.read().decode('utf-8-sig')
         template = json.loads(data)
-    return set(k.upper() for k in template.get('parameters', {}).keys())
+    return list(k.upper() for k in template.get('parameters', {}).keys())
 
 def compare_parameters(maps_template_url, quickstart_template_url): 
     maps_params = load_parameters_from_url(maps_template_url)
@@ -13,9 +13,9 @@ def compare_parameters(maps_template_url, quickstart_template_url):
 
     additional_params = quickstart_params - maps_params
 
-    print ("Custom params:", sorted(maps_params))
-    print ("QS params:", sorted(quickstart_params))
-    print ("Suggested params:", sorted(additional_params))
+    print ("Custom params:", maps_params.sort())
+    print ("QS params:", quickstart_params.sort())
+    print ("Suggested params:", additional_params.sort())
 
 # Call the function with URLs
 compare_parameters(
